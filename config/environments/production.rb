df25b712eda32d6998b1d88b0848b7f506dcb311
Hendra Nicholas <hendra@41studio.com>
2013-01-06 17:39:08 +0700

    rotating production file log

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ Brew::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new(Rails.root.join("log", Rails.env + ".log"), 10, 5*1024*1024)
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
